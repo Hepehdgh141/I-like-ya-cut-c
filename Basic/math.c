dff566185ae09adfbdcd299e716d0c276b36a54d
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-03-11 21:12:46 +0900

    Basic.Bonus1

---
+++ Basic/math.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  double square, fab, powe, ascos, assin, astan, astan2, coss, sinn, tann;
+  
+  square = sqrt(1.0) // 루트
+  fab = fabs(-9.0) // abs(-9.0)도 된다. 절댓값
+  powe = pow(8.0, 9.0) // 8^9. 제곱
+  ascos = acos(1.0) // 아크 코사인
+  
+}
