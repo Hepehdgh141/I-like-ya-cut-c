2455b1f04ea6fa6fedd9964e4ac8df7c940e8b5d
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-03-11 21:25:37 +0900

    Basic.Bonus1

--- Basic/math.c
+++ Basic/math.c
@@ -5,9 +5,10 @@
 int main() {
   double square, fab, powe, ascos, assin, astan, astan2, coss, sinn, tann;
   
-  square = sqrt(1.0) // 루트
-  fab = fabs(-9.0) // abs(-9.0)도 된다. 절댓값
-  powe = pow(8.0, 9.0) // 8^9. 제곱
-  ascos = acos(1.0) // 아크 코사인
+  square = sqrt(1.0); // 루트
+  fab = fabs(-9.0); // abs(-9.0)도 된다. 절댓값
+  powe = pow(8.0, 9.0); // 8^9. 제곱
+  ascos = acos(1.0); // 아크 코사인
   
+  return 0;
 }
