ae7323b3473e327127bc85621c795af27649f61a
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-03-11 21:48:21 +0900

    Basic.No5

--- Basic/Printf.c
+++ Basic/Printf.c
@@ -7,9 +7,15 @@ int main() {
   printf("%g", 0.123);  // 실수 float 하지만 그냥 출력
   printf("%c", 'a'); // 문자 char
   printf("%s", "Hello guys"); // 문자열 string 근데 굳이?
-  printf("\n \t \\ \" \' %%") 
+  printf("\n \t \\ \" \' %%");
   /*
   \n은 줄바꿈, \t은 tab키, \\은 \ 출력, \"는 " 를출력, \'는 '를 출력한다.
   하지만 예외로 %%는 %를 출력한다.  
   */
+   
+  // 아래의 코드는 cast 연산자이다.
+  int a = 65;
+  printf("%c", (char)a); // 임시적으로 자료형을 바꿔주는건데, int에서 char로 바꾸면 ASCII 값을 바탕으로 바뀐다.
+  
+  return 0;
 }
