4dc296939443b4b79e711f63b4e8780800d62aa9
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-03-17 20:15:57 +0900

    Basic.Bonus4

--- Basic/hanoitower.c
+++ Basic/hanoitower.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int a = 0;
+
 int main() {
 	int num, from, to, other;
 	
@@ -20,22 +22,13 @@ int main() {
 }
 
 hanoiTower(number, from, to, other) {
-  // number: 원반의 수
-  // from: 원반들이 위치한 곳의 번호
-  // to: 원반들을 옮길 목적지 번호
-  // other: 나머지 한 곳(목적지가 아닌) 곳 번호
-
-  // 모두 옮겼으면 종료
-  if (number == 0) {
-  	return;	
-  }
-
-  // 가장 아래 원반을 제외한 원반들을 재귀적으로 목적지가 아닌 곳으로 옮김
+  if (number == 0) return;	
+	
   hanoiTower(number - 1, from, other, to);
+  
+  getchar();
+  
+  printf("%d번칸에서 %d번칸으로 옮긴다. (%d번째 동작)\n \n", from + 1, to + 1, (a++) + 1);
 
-  // 가장 아래 원반을 목적지로 옮김
-  printf("%d번칸에서 %d번칸으로 옮긴다.\n \n", from + 1, to + 1);
-
-  // 목적지가 아닌 곳으로 옮겼던 원반들을 재귀적으로 목적지로 옮김
   hanoiTower(number - 1, other, to, from);
 }
