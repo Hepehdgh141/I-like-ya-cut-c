dc5f0c11276f6921c4acbc7159232caa4ca6a694
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-03-27 15:05:24 +0900

    Advanced.No1

--- Advanced/RecursiveFunction.c
+++ Advanced/RecursiveFunction.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int FibonaciFunc(int n);
+
+int FibonaciFunc(int n) { 
+    if (n == 1) return 1; // 재귀함수가 끝나는 조건
+    return n * factorial(n - 1); // 함수 안에 함수 값
+}
+
+int main() {
+  FibonaciFunc(32)
+}
