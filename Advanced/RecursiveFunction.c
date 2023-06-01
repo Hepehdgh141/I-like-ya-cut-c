4a87a0ebe8521a9d2e9ebf2678a941c28caa7c52
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-06-01 22:28:57 +0900

    Advanced.No1

--- Advanced/RecursiveFunction.c
+++ Advanced/RecursiveFunction.c
@@ -5,14 +5,14 @@ int Count(int n);
 
 int Count(int n) {
     if (n == 0){ // 끝나는 조건. n이 0이 될때, 즉 끝나는 조건.
-    	printf("\nROUND ENDED"); // 굳이 안 넣어도 되지만 그냥 넣었다
+    	printf("\nROUND ENDED");
 	return 0;
     }
     printf("%d SECONDS REMAINS", n);
     n--;
     Sleep(1000)
     printf("\n");
-    Count(n); // 이렇게 for문이나 while문 대신 이렇게 재귀함수를 써서 쓸 수 있다.
+    Count(n); // 이렇게 for문이나 while문 대신 이렇게 재귀함수를 써서 쓸 수 있다. 백준 같은걸 풀때 시간/메모리 상으로 써야하는 경우도 종종 있다.
 }
 
 int main() {
