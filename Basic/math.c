4da5c03200491643b5ab765c15d21a8d742c75aa
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-06-02 22:51:26 +0900

    Basic.Bonus1

--- Basic/math.c
+++ Basic/math.c
@@ -9,6 +9,8 @@ int main() {
   fab = fabs(-9.0); // abs(-9.0)도 된다. 절댓값
   powe = pow(8.0, 9.0); // 8^9. 제곱
   ascos = acos(1.0); // 아크 코사인
+
+  printf("%g, %g, %lf, %g", square, fab, powe, ascos);
   
   return 0;
 }
