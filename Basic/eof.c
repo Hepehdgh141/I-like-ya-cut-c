05dc24c670fe7157c38ace5008f5bfe32f412cba
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-06-02 23:04:55 +0900

    Basic.Bonus3

--- Basic/eof.c
+++ Basic/eof.c
@@ -1,20 +1,20 @@
-// 이 프로그램은 ^Z(ctrl+z)를 누를때 종료되는 while문을 응용한 프로그램이다.
+// 이 프로그램은 ^Z(ctrl+z), ^D(ctrl+d)를 누를때 종료되는 while문을 응용한 프로그램이다.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 int main() {
-	int a;
-	while (true) {
-		a = scanf("%d", &a); // a = getchar();
-		printf("%d", a);
-		if (a == EOF) // EOF는 end of file의 약자이다. C언어에선 -1
-		{
-			break;
-		}
-		putchar(a); // 숫자를 ASCII로 바꿔준다.
-	}
-	
-	return 0;
+    int a = 0;
+    while (true) {
+        if (scanf("%d", &a) != 1) {
+            break;
+        }
+        if (a == EOF) {
+            break;
+        }
+        printf("%d\n", a);
+    }
+    
+    return 0;
 }
