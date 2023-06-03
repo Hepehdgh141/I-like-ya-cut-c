f019758ed32c73f0ddad434ecac39571dcead7ee
Hepehdgh141 <72325653+Hepehdgh141@users.noreply.github.com>
2023-06-03 09:12:18 +0900

    Basic.No4

--- Basic/Define.c
+++ Basic/Define.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define pi 3.141592 // 상수 #undef pi로 해제 할 수 있다
+#define pi 3.141592 
+// 상수 #undef pi로 해제 할 수 있다
 
 int main() {
-  print("%f", pi);
-#undef pi
-#def pi 3.14159
+  printf("%f\n", pi);
+  #undef pi
+  #define pi 3.14159
   printf("%f", pi);
   return 0;
 }
